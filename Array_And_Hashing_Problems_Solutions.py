b864238878d38ba15febe6df457c18c72b75570e
Omar Mahrous <omer.mohamed.mahrous@gmail.com>
2025-07-16 17:26:11 +0300

    Update Array_And_Hashing_Problems_Solutions.py

--- Array_And_Hashing_Problems_Solutions.py
+++ Array_And_Hashing_Problems_Solutions.py
@@ -125,3 +125,33 @@ secret_string=test.encode(["neet","code","love","you"])
 print(secret_string)
 
 original_string=test.decode(secret_string)
+
+
+#6.Product of Array Except Self 
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res=[1]*len(nums)
+
+
+        prefix=1
+        for i in range(len(nums)):
+            res[i]=prefix
+            prefix*=nums[i]
+
+
+        postfix=1
+        for i in range(len(nums)-1,-1,-1):
+            res[i]*=postfix
+            postfix*=nums[i]
+
+        return res
+
+
+sol = Solution()
+nums = [1, 2, 3, 4]
+result = sol.productExceptSelf(nums)
+print(result)
+       
+            
+        
